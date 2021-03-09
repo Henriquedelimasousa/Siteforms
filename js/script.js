3e4a0911f342ec04c57506d496882415c0df050d
Henrique Sousa <72755928+Henriquedelimasousa@users.noreply.github.com>
2021-03-09 00:42:47 -0300

    Adicionei
    Atividade 1° entregável

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+var fnome = document.getElementById('fnome');
+
+function enviar() {
+
+    if (fnome.value != "") {
+        alert('Obrigado sr(a) ' + fnome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+}
